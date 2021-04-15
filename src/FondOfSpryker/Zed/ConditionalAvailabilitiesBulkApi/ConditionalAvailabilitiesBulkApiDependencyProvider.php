d0bc5258ff5333b5a45bd43c03c5ade70d2bd13a
Jozsef Geng <jozsef.geng@fondof.de>
2021-04-15 17:16:09 +0200

    add ConditionalAvailabilityFacade in the dependency provider

--- src/FondOfSpryker/Zed/ConditionalAvailabilitiesBulkApi/ConditionalAvailabilitiesBulkApiDependencyProvider.php
+++ src/FondOfSpryker/Zed/ConditionalAvailabilitiesBulkApi/ConditionalAvailabilitiesBulkApiDependencyProvider.php
@@ -2,14 +2,41 @@
 
 namespace FondOfSpryker\Zed\ConditionalAvailabilitiesBulkApi;
 
-use FondOfSpryker\Zed\ConditionalAvailabilityApi\Dependency\Facade\ConditionalAvailabilityApiToConditionalAvailabilityFacadeBridge;
-use FondOfSpryker\Zed\ConditionalAvailabilityApi\Dependency\QueryContainer\ConditionalAvailabilityApiToApiQueryBuilderQueryContainerBridge;
-use FondOfSpryker\Zed\ConditionalAvailabilityApi\Dependency\QueryContainer\ConditionalAvailabilityApiToApiQueryContainerBridge;
-use Orm\Zed\ConditionalAvailability\Persistence\FosConditionalAvailabilityQuery;
+use FondOfSpryker\Zed\ConditionalAvailabilitiesBulkApi\Dependency\Facade\ConditionalAvailabilitesBulkApiToConditionalAvailabilityFacadeBridge;
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Zed\Kernel\Container;
 
 class ConditionalAvailabilitiesBulkApiDependencyProvider extends AbstractBundleDependencyProvider
 {
-   
+    public const FACADE_CONDITIONAL_AVAILABILITY = 'FACADE_CONDITIONAL_AVAILABILITY';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container): Container
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+
+        $container = $this->addConditionalAvailabilityFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addConditionalAvailabilityFacade(Container $container): Container
+    {
+        $container[static::FACADE_CONDITIONAL_AVAILABILITY] = static function (Container $container) {
+            return new ConditionalAvailabilitesBulkApiToConditionalAvailabilityFacadeBridge(
+                $container->getLocator()->conditionalAvailability()->facade()
+            );
+        };
+
+        return $container;
+    }
 }
