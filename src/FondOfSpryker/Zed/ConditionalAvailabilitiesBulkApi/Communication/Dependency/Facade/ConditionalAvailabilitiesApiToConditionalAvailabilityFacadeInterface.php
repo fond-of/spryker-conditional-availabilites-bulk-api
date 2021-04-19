2f466e0d39ac522629d814baeb11e2d3460a577a
Jozsef Geng <jozsef.geng@fondof.de>
2021-04-19 10:24:17 +0200

    add Create/Update method in ConditionalAvailabilityFacadeInterface

--- src/FondOfSpryker/Zed/ConditionalAvailabilitiesBulkApi/Communication/Dependency/Facade/ConditionalAvailabilitiesApiToConditionalAvailabilityFacadeInterface.php
+++ src/FondOfSpryker/Zed/ConditionalAvailabilitiesBulkApi/Communication/Dependency/Facade/ConditionalAvailabilitiesApiToConditionalAvailabilityFacadeInterface.php
@@ -4,12 +4,31 @@ namespace FondOfSpryker\Zed\ConditionalAvailabilitiesBulkApi\Dependency\Facade;
 
 use Generated\Shared\Transfer\ConditionalAvailabilityCollectionTransfer;
 use Generated\Shared\Transfer\ConditionalAvailabilityCriteriaFilterTransfer;
+use Generated\Shared\Transfer\ConditionalAvailabilityResponseTransfer;
+use Generated\Shared\Transfer\ConditionalAvailabilityTransfer;
 
 interface ConditionalAvailabilitiesApiToConditionalAvailabilityFacadeInterface
 {
     /**
+     * @param \Generated\Shared\Transfer\ConditionalAvailabilityTransfer $conditionalAvailabilityTransfer
+     *
+     * @return \Generated\Shared\Transfer\ConditionalAvailabilityResponseTransfer
+     */
+    public function createConditionalAvailability(
+        ConditionalAvailabilityTransfer $conditionalAvailabilityTransfer
+    ): ConditionalAvailabilityResponseTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\ConditionalAvailabilityTransfer $conditionalAvailabilityTransfer
+     * @return \Generated\Shared\Transfer\ConditionalAvailabilityResponseTransfer
+     */
+    public function updateConditionalAvailability(
+        ConditionalAvailabilityTransfer $conditionalAvailabilityTransfer
+    ): ConditionalAvailabilityResponseTransfer;
+
+    /**
      * @param \Generated\Shared\Transfer\ConditionalAvailabilityCriteriaFilterTransfer $conditionalAvailabilityCriteriaFilterTransfer
-     * 
+     *
      * @return \Generated\Shared\Transfer\ConditionalAvailabilityCollectionTransfer
      */
     public function findConditionalAvailabilities(
